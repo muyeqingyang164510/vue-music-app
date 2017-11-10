cb2313c633a09106f202990740391da9ffbc9234
muyeqingyang164510 <15038977762@163.com>
2017-11-10 14:28:20 +0800

    提交vue-music-app-1

---
+++ src/base/scroll/scroll.vue
@@ -0,0 +1,68 @@
+<template>
+  <div ref="wrapper">
+    <slot></slot>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  import BScroll from 'better-scroll'
+
+  export default {
+    props: {
+      probeType: {
+        type: Number,
+        default: 1
+      },
+      click: {
+        type: Boolean,
+        default: false
+      },
+      data: {
+        type: Array,
+        default: null
+      }
+    },
+    mounted() {
+      setTimeout(() => {
+        this._initScroll()
+      }, 20)
+    },
+    methods: {
+      _initScroll() {
+        if (!this.$refs.wrapper) {
+          return
+        }
+        this.scroll = new BScroll(this.$refs.wrapper, {
+          probeType: this.probeType,
+          click: this.click
+        })
+      },
+      disable() {
+        this.scroll && this.scroll.disable()
+      },
+      enable() {
+        this.scroll && this.scroll.enable()
+      },
+      refresh() {
+        this.scroll && this.scroll.refresh()
+      },
+      scrollTo() {
+        this.scroll && this.scroll.scrollTo.apply(this.scroll, arguments)
+      },
+      scrollToElement() {
+        this.scroll && this.scroll.scrollToElement.apply(this.scroll, arguments)
+      }
+    },
+    watch: { // data数据变化，重新刷新
+      data() {
+        setTimeout(() => {
+          this.refresh()
+        }, 20)
+      }
+    }
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,25 @@
+import 'babel-polyfill'
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import fastclick from 'fastclick'
+import VueLazyLoad from 'vue-lazyload' // 懒加载插件
+
+import 'common/stylus/index.styl'
+fastclick.attach(document.body)
+Vue.use(VueLazyLoad, {
+  loading: require('common/image/default.png')
+})
+/* eslint-disable no-new */
+// 其实只是简写的方式。
+// 如果不涉及使用this的话下面这个也是等价的
+// {
+//     render: function (h) {
+//         return h(App)  h 为 createElement 函数
+//     }
+// }
+new Vue({
+  el: '#app',
+  render: h => h(App),
+  router
+})
